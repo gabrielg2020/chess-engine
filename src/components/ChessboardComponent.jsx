15a350652ba337342f8b92af78edabcfaafd8695
Gabriel Guimaraes <gabriel.mg04@outlook.com>
2024-05-16 00:09:45 +0100

    feat: correct piece highlighting

--- src/components/ChessboardComponent.jsx
+++ src/components/ChessboardComponent.jsx
@@ -6,6 +6,7 @@ const ChessboardComponent = () => {
   const [chess] = useState(new Chess());
   const [fen, setFen] = useState('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR');
   const [prevFen, setPrevFen] = useState('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR');
+  const [squareStyles, setSquareStyles] = useState({});
 
   useEffect(() => {
     setPrevFen(fen);
@@ -26,12 +27,45 @@ const ChessboardComponent = () => {
     }
   };
 
+  const highlightSquare = (square) => {
+    const piece = chess.get(square);
+    setSquareStyles({});
+    
+    // Check if selected piece is the correct color
+    if (chess.turn() !== piece.color) return;
+
+    // Highlight clicked square
+    let highlightStyles = {[square]: { background: 'rgba(255, 255, 0, 0.4)' }}
+
+    // Highlight available moves
+    const moves = chess.moves({
+      square,
+      verbose: true
+    });
+
+    moves.forEach((move) => {
+      let color = 'rgba(100, 255, 0, 0.4'
+      if (move.flags === 'c'){
+        color = 'rgba(255, 100, 0, 0.4'
+      }
+
+      highlightStyles = {
+        ...highlightStyles,
+        [move.to]: { background: color }
+      }
+    })
+
+    setSquareStyles(highlightStyles);
+  };
+
   return (
     <div>
       <Chessboard 
         draggable={true}
         onDrop={(move) => handleMove(move)}
         position={fen} // initial position in FEN
+        onMouseOverSquare={(square) => highlightSquare(square)}
+        squareStyles={squareStyles}
       />
     </div>
   );
