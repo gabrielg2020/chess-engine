e30df2f65e6b36bbb3bf81f85d931cc6db0882c8
Gabriel Guimaraes <gabriel.mg04@outlook.com>
2024-05-15 22:56:52 +0100

    feat: can move pieices

--- src/components/ChessboardComponent.jsx
+++ src/components/ChessboardComponent.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Chessboard from 'chessboardjsx';
+import { Chess }from 'chess.js';
 
 const ChessboardComponent = () => {
+  const [chess] = useState(new Chess());
+  const [fen, setFen] = useState('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR')
+
+  const handleMove = ({ sourceSquare, targetSquare, piece }) => {
+    const game = chess;
+    const move = game.move({
+      from: sourceSquare,
+      to: targetSquare,
+      promotion: 'q'
+    });
+
+    if (move == null) return;
+    setFen(game.fen());
+  };
+
   return (
     <div>
       <Chessboard 
-        position="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR" // initial position in FEN
+        draggable={true}
+        onDrop={(move) => handleMove(move)}
+        position={fen} // initial position in FEN
       />
     </div>
   );
